0e8d5e26f31bfcb0e00aff4326246fdb2d6195c1
Rishav Mondal <mondalrishav25@gmail.com>
2025-03-03 18:26:54 +0530

    Update layout.tsx

--- src/app/studio/layout.tsx
+++ src/app/studio/layout.tsx
@@ -1,6 +1,4 @@
 import HeaderStudio from "@/components/StudioComps/Headers/HeaderStudio";
-import SidebarContainer from "@/components/StudioComps/SidebarComps/SidebarContainer";
-import { SidebarProvider } from "@/components/ui/sidebar";
 import { LayoutProps } from "@/lib/types";
 
 const StudioLayout = ({ children }: LayoutProps) => {
