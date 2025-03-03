c321762a6d32b633ed4ac899fc5023733e1fe7f8
Rishav Mondal <mondalrishav25@gmail.com>
2025-03-03 17:55:58 +0530

    confusion in sidebar

--- src/app/studio/layout.tsx
+++ src/app/studio/layout.tsx
@@ -6,15 +6,9 @@ import { LayoutProps } from "@/lib/types";
 const StudioLayout = ({ children }: LayoutProps) => {
   return (
     <>
-      <SidebarProvider>
-        <SidebarContainer />
+      <HeaderStudio />
 
-        <section className="w-full">
-          <HeaderStudio />
-
-          <main className="container mx-auto px-6">{children}</main>
-        </section>
-      </SidebarProvider>
+      <main className="container mx-auto px-6">{children}</main>
     </>
   );
 };
--- src/components/StudioComps/Headers/HeaderStudio.tsx
+++ src/components/StudioComps/Headers/HeaderStudio.tsx
@@ -3,13 +3,15 @@ import ThemeToggleButton from "@/components/ui/ThemeToggleButton";
 const HeaderStudio = () => {
   return (
     <>
-      <div className="border-foreground/10 container mx-auto flex items-center justify-between border-b px-6 py-3 shadow">
-        <div className="text-2xl font-bold">Dashboard</div>
+      <header className="border-foreground/10 border-b shadow">
+        <div className="container mx-auto flex items-center justify-between px-6 py-3">
+          <div className="text-2xl font-bold">Dashboard</div>
 
-        <div className="">
-          <ThemeToggleButton />
+          <div className="">
+            <ThemeToggleButton />
+          </div>
         </div>
-      </div>
+      </header>
     </>
   );
 };
